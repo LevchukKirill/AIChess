5210cf435e02e2a201116a699abd9e76322619f4
Kirill Levchuk <kirya.levch@mail.ru>
2024-06-20 18:14:16 +0200

    added menu slide list

--- web-chess/src/components/layouts/header/Header.tsx
+++ web-chess/src/components/layouts/header/Header.tsx
@@ -3,23 +3,44 @@ import styles from './header.module.css';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
+    <header className={styles.header} tabIndex={0}>
       {/* <div className={styles.container}> */}
-      <nav id='container'>
-        <ul>
-          <a href=''>
-            <li className={styles.nav_li}>Home</li>
-          </a>
-        </ul>
-      </nav>
+      {/* <div className={styles.nav_container}> */}
+      <div id={styles.nav_container}>
+        <div className={styles.bg}></div>
+        <div className={styles.button} tabIndex={0}>
+          <div className={styles.nav_el}>Home</div>
+        </div>
+        <div id={styles.menu_list} tabIndex={0}>
+          <ul>
+            <li>
+              <a href='#0'>Home</a>
+            </li>
+            <li>
+              <a href='#0'>Services</a>
+            </li>
+            <li>
+              <a href='#0'>Blog</a>
+            </li>
+            <li>
+              <a href='#0'>About</a>
+            </li>
+            <li>
+              <a href='#0'>Contact</a>
+            </li>
+            <li>
+              <a href='#0'>Facebook</a>
+              <a href='#0'>Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <h1 className={styles.header_text}>Chess AI</h1>
-      <nav className={styles.nav}>
-        <ul>
-          <a href=''>
-            <li className={styles.nav_li}>User</li>
-          </a>
-        </ul>
-      </nav>
+      <div className={styles.container_right}>
+        <div className={styles.button}>
+          <div className={styles.nav_el}>User</div>
+        </div>
+      </div>
       {/* </div> */}
     </header>
   );
