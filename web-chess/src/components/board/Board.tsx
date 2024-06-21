37788f743bced55b050b649209e378b85976d2e6
Kirill Levchuk <kirya.levch@mail.ru>
2024-06-21 03:21:32 +0200

    added assest (logo, chesspiece), added board axes and squares

--- web-chess/src/components/board/Board.tsx
+++ web-chess/src/components/board/Board.tsx
@@ -1,6 +1,44 @@
-import styles from "./board.module.css";
+import React from 'react';
+import styles from './board.module.css';
+
+const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
 const Board = () => {
-  return <div className={styles.board}>ya big board</div>;
+  let board: object[] = [];
+  console.log(verticalAxis.length);
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const numbers = j + i + 2;
+      if (numbers % 2 === 0) {
+        board.push(
+          <div
+            key={verticalAxis[i]}
+            className={`${styles.tile}, ${styles.black}`}
+          >
+            [{horizontalAxis[i]}
+            {verticalAxis[j]}]
+          </div>
+        );
+      } else {
+        board.push(
+          <div
+            key={verticalAxis[i]}
+            className={`${styles.tile}, ${styles.white}`}
+          >
+            [{horizontalAxis[i]}
+            {verticalAxis[j]}]
+          </div>
+        );
+      }
+    }
+  }
+
+  return (
+    <div className={styles.board}>
+      <>{board}</>
+    </div>
+  );
 };
 
 export default Board;
--- web-chess/src/components/templates/game.tsx
+++ web-chess/src/components/templates/game.tsx
@@ -1,7 +1,7 @@
-import styles from "./game.module.css";
-import Chat from "../chat/Chat";
-import Board from "../board/Board";
-import UsersInfo from "../usersInfo/UsersInfo";
+import styles from './game.module.css';
+import Chat from '../chat/Chat';
+import Board from '../board/Board';
+import UsersInfo from '../usersInfo/UsersInfo';
 const Game = () => {
   return (
     <div className={styles.main}>
