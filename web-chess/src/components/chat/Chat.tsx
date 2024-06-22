9ba1a176bf174c178b94a2c79f8485d9560e1477
Nikita <nikimonviad@mail.ru>
2024-06-22 20:20:51 +0200

    chat and userInfo changes

--- web-chess/src/components/chat/Chat.tsx
+++ web-chess/src/components/chat/Chat.tsx
@@ -12,7 +12,7 @@ const Chat = () => {
             890123 456789
           </h5>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row m-b-1">
           <h5 className={styles.enemy__message}>
             Сообщение 01234567890123456789012345678901234567890
             1234567890123456789012345678901234567890123456789012345678901234567
@@ -28,8 +28,59 @@ const Chat = () => {
           <div className={styles.user__icon__mini}>ic1</div>
           <h5 className={styles.player__message}>Сообщение 4</h5>
         </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
+        <div className="flex flex-row">
+          <div className={styles.user__icon__mini}>ic1</div>
+          <h5 className={styles.player__message}>Сообщение 4</h5>
+        </div>
       </div>
-      <div className={styles.chat__input}></div>
+      <input
+        placeholder="пиши в чат лох"
+        className={styles.chat__input}
+      ></input>
     </div>
   );
 };
--- web-chess/src/components/usersInfo/UsersInfo.tsx
+++ web-chess/src/components/usersInfo/UsersInfo.tsx
@@ -1,24 +1,24 @@
 import styles from "./usersInfo.module.css";
 const UsersInfo = () => {
   return (
-    <div className={styles.users}>
+    <div className={styles.main}>
       <div className={styles.user__top}>
         <div className="flex flex-row">
-          <div className={styles.user__icon}>ic1</div>
-          <div className={styles.username}>user1</div>
+          <div className={styles.user__icon}></div>
+          <div className={styles.username}>NikiMon</div>
         </div>
         <div className="flex flex-row">
-          <div className={styles.rating}>350 elo</div>
+          <div className={styles.rating}>35 elo</div>
           <div className={styles.figures}>pawn pawn knight</div>
         </div>
       </div>
       <div className={styles.moves}></div>
       <div className={styles.user__bottom}>
-        <div className="flex flex-row">
-          <div className={styles.user__icon}>ic2</div>
-          <div className={styles.username}>user2</div>
+        <div className="flex flex-column">
+          <div className={styles.user__icon}></div>
+          <div className={styles.username}>velikiyurod</div>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-column align-center">
           <div className={styles.rating}>2000 elo</div>
           <div className={styles.figures}>pawn</div>
         </div>
